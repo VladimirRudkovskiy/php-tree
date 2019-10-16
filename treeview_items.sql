7d2c319e9ceb40c05739f6b1cae260d3666dbba7
Vladimir <ryan_vancity_crawford@mail.ru>
2019-10-16 16:10:59 +0300

    tree items correct

--- treeview_items.sql
+++ treeview_items.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Sep 29, 2015 at 11:08 AM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
